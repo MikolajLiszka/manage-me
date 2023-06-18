fe7e65791e337a4bce87b1f1af3d48a57ad686be
MikolajLiszka <mikolaj.liszka@gmail.com>
2023-06-18 15:00:52 +0200

    crud2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtnCreateNewProjComponent } from './components/btn-create-new-proj/btn-create-new-proj.component';
 import { NewProjectsComponent } from './components/new-projects/new-projects.component';
+import { NewProjectFormComponent } from './components/new-project-form/new-project-form.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BtnCreateNewProjComponent,
-    NewProjectsComponent
+    NewProjectsComponent,
+    NewProjectFormComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/new-project-form/new-project-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-project-form',
+  templateUrl: './new-project-form.component.html',
+  styleUrls: ['./new-project-form.component.scss']
+})
+export class NewProjectFormComponent {
+
+}
