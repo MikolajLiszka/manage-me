2152fe28a1e7e0923da33a9caa1715c951d678a7
MikolajLiszka <mikolaj.liszka@gmail.com>
2023-06-20 17:57:47 +0200

    local storage

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+
 import { Injectable } from '@angular/core';
 import { Project } from '../models/project.model';
 
@@ -6,34 +6,52 @@ import { Project } from '../models/project.model';
   providedIn: 'root'
 })
 export class ProjectService {
-  private projects: Project[] = [];
+  private projectIdCounter: number = 0;
 
-
-  constructor(private router: Router) {}
+  constructor() {}
 
   getProjects(): Project[] {
-    return this.projects;
+    return this.getAllProjects();
   }
 
   getProjectById(projectId: number): Project | undefined {
-    return this.projects.find(project => project.id === projectId);
+    const projects = this.getAllProjects();
+    return projects.find(project => project.id === projectId);
   }
 
   getAllProjects(): Project[] {
-    return this.projects;
+    const storedProjects = localStorage.getItem('projects');
+    if (storedProjects) {
+      return JSON.parse(storedProjects);
+    } else {
+      return [];
+    }
   }
 
   addProject(project: Project): void {
-    this.projects.push(project);
+    const projects = this.getAllProjects();
+    project.id = this.generateUniqueId();
+    projects.push(project);
+    this.saveProjectsToLocalStorage(projects);
   }
 
   deleteProject(id: number): void {
-    this.projects = this.projects.filter(p => p.id !== id);
-    }
-
-  projectDetails(projectId: number) {
-    this.router.navigate(['/projects', projectId]);
+    let projects = this.getAllProjects();
+    projects = projects.filter(p => p.id !== id);
+    this.saveProjectsToLocalStorage(projects);
   }
-  
+
+  projectDetails(projectId: number): void {
+    // You can add the routing logic here
+  }
+
+  private generateUniqueId(): number {
+    this.projectIdCounter++;
+    return this.projectIdCounter;
+  }
+
+  private saveProjectsToLocalStorage(projects: Project[]): void {
+    localStorage.setItem('projects', JSON.stringify(projects));
+  }
 }
 
